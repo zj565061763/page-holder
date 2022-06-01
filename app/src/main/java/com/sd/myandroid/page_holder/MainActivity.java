337aacc2a3ed8c65885388664da7ebdb0f207ed4
zhengjun <565061763@qq.com>
2022-06-01 13:01:25 +0800

    testRefresh

--- app/src/main/java/com/sd/myandroid/page_holder/MainActivity.java
+++ app/src/main/java/com/sd/myandroid/page_holder/MainActivity.java
@@ -6,43 +6,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.sd.lib.holder.page.FPageHolder;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test();
+        testRefresh();
     }
 
-    private void test() {
+    private void testRefresh() {
         final FPageHolder holder = new FPageHolder();
 
+        // test init
         assert holder.getCurrentPage() == 0;
         assert holder.getPageForRefresh() == 1;
         assert holder.getPageForLoadMore() == 1;
         assert !holder.hasNextPage();
 
-        holder.onSuccess(false)
-                .setHasData(false)
-                .setHasNextPage(true)
-                .update();
 
+        // test setHasNextPage(true)
+        holder.onSuccess(false)
+                .setHasNextPage(true)
+                .setHasData(false)
+                .update();
         assert holder.getCurrentPage() == 0;
         assert holder.getPageForRefresh() == 1;
         assert holder.getPageForLoadMore() == 1;
         assert holder.hasNextPage();
 
-        holder.onSuccess(false)
-                .setHasData(true)
-                .setHasNextPage(true)
-                .update();
 
+        // test setHasNextPage(false)
+        holder.onSuccess(false)
+                .setHasNextPage(false)
+                .setHasData(false)
+                .update();
+        assert holder.getCurrentPage() == 0;
+        assert holder.getPageForRefresh() == 1;
+        assert holder.getPageForLoadMore() == 1;
+        assert !holder.hasNextPage();
+
+
+        // test setHasData(true)
+        holder.onSuccess(false)
+                .setHasNextPage(true)
+                .setHasData(true)
+                .update();
         assert holder.getCurrentPage() == 1;
         assert holder.getPageForRefresh() == 1;
         assert holder.getPageForLoadMore() == 2;
-        assert holder.hasNextPage();
 
 
+        // test setHasData(false)
+        holder.onSuccess(false)
+                .setHasNextPage(true)
+                .setHasData(false)
+                .update();
+        assert holder.getCurrentPage() == 0;
+        assert holder.getPageForRefresh() == 1;
+        assert holder.getPageForLoadMore() == 1;
+
+
+        // test setHasData(Collection)
+        holder.onSuccess(false)
+                .setHasNextPage(true)
+                .setHasData(new ArrayList<>())
+                .update();
+        assert holder.getCurrentPage() == 0;
+        assert holder.getPageForRefresh() == 1;
+        assert holder.getPageForLoadMore() == 1;
     }
 }
