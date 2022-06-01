a87c7b33ae3763f9341a83c5c597153eabf83dfa
zhengjun <565061763@qq.com>
2022-06-01 12:04:41 +0800

    test

--- app/src/main/java/com/sd/myandroid/page_holder/MainActivity.java
+++ app/src/main/java/com/sd/myandroid/page_holder/MainActivity.java
@@ -7,14 +7,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.sd.lib.holder.page.FPageHolder;
 
 public class MainActivity extends AppCompatActivity {
-    private final FPageHolder mPageHolder = new FPageHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
 
-        assert mPageHolder.getPageForRefresh() == 1;
-        assert mPageHolder.getPageForLoadMore() == 1;
+    private void test() {
+        final FPageHolder holder = new FPageHolder();
+
+        assert holder.getCurrentPage() == 0;
+        assert holder.getPageForRefresh() == 1;
+        assert holder.getPageForLoadMore() == 1;
+        assert holder.getPageForRequest(false) == 1;
+        assert holder.getPageForRequest(true) == 1;
+        assert !holder.hasNextPage();
+
+        holder.onSuccess(false)
+                .setHasData(true)
+                .setHasNextPage(true)
+                .update();
+
+        assert holder.getCurrentPage() == 1;
+        assert holder.getPageForRefresh() == 1;
+        assert holder.getPageForLoadMore() == 2;
+        assert holder.getPageForRequest(false) == 1;
+        assert holder.getPageForRequest(true) == 2;
+        assert holder.hasNextPage();
     }
 }
