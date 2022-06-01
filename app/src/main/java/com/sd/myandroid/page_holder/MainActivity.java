0257d06159f75c41323e9aac9ead51d7c735ce34
zhengjun <565061763@qq.com>
2022-06-01 11:58:59 +0800

    demo

--- app/src/main/java/com/sd/myandroid/page_holder/MainActivity.java
+++ app/src/main/java/com/sd/myandroid/page_holder/MainActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sd.lib.holder.page.FPageHolder;
+
 public class MainActivity extends AppCompatActivity {
+    private final FPageHolder mPageHolder = new FPageHolder();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        assert mPageHolder.getPageForRefresh() == 1;
+        assert mPageHolder.getPageForLoadMore() == 1;
     }
 }
