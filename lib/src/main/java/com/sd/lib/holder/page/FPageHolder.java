429151534f8ec09f77389d0b0837f54edb75b0f8
zhengjun <565061763@qq.com>
2022-06-01 11:39:23 +0800

    FPageHolder

--- lib/src/main/java/com/sd/lib/holder/page/FPageHolder.java
+++ lib/src/main/java/com/sd/lib/holder/page/FPageHolder.java
@@ -1,64 +1,37 @@
 package com.sd.lib.holder.page;
 
-import java.util.List;
+import java.util.Collection;
 
 /**
  * 分页逻辑封装
  */
-public class FPageHolder
-{
-    private int mCurrentPage;
-    private int mCurrentCount;
-    private boolean mHasNextPage;
-
-    public FPageHolder()
-    {
-        reset();
-    }
+public class FPageHolder {
+    private volatile int mCurrentPage = 0;
+    private boolean mHasNextPage = false;
 
     /**
      * 是否有下一页数据
-     *
-     * @return
      */
-    public boolean hasNextPage()
-    {
+    public boolean hasNextPage() {
         return mHasNextPage;
     }
 
     /**
      * 返回当前的页数
-     *
-     * @return
      */
-    public int getCurrentPage()
-    {
+    public int getCurrentPage() {
         return mCurrentPage;
-    }
-
-    /**
-     * 返回当前数据的数量
-     *
-     * @return
-     */
-    public int getCurrentCount()
-    {
-        return mCurrentCount;
     }
 
     /**
      * 返回当前请求需要传入的page
      *
      * @param isLoadMore 是否加载更多
-     * @return
      */
-    public int getPageForRequest(boolean isLoadMore)
-    {
-        if (isLoadMore)
-        {
+    public int getPageForRequest(boolean isLoadMore) {
+        if (isLoadMore) {
             return mCurrentPage + 1;
-        } else
-        {
+        } else {
             return 1;
         }
     }
@@ -66,62 +39,44 @@ public class FPageHolder
     /**
      * 接口成功触发
      *
-     * @param isLoadMore
-     * @return
+     * @param isLoadMore 是否加载更多
      */
-    public synchronized ResultUpdater onSuccess(boolean isLoadMore)
-    {
+    public ResultUpdater onSuccess(boolean isLoadMore) {
         return new ResultUpdater(isLoadMore);
     }
 
-    private synchronized void updatePage(boolean isLoadMore, boolean hasNextPage, boolean hasData)
-    {
+    private synchronized void updatePage(boolean isLoadMore, boolean hasNextPage, boolean hasData) {
         mHasNextPage = hasNextPage;
 
-        if (isLoadMore)
-        {
-            if (hasData)
+        if (isLoadMore) {
+            if (hasData) {
                 mCurrentPage++;
-        } else
-        {
-            if (hasData)
+            }
+        } else {
+            if (hasData) {
                 mCurrentPage = 1;
-            else
+            } else {
                 mCurrentPage = 0;
+            }
         }
     }
 
-    /**
-     * 重置
-     */
-    public synchronized void reset()
-    {
-        mCurrentPage = 0;
-        mCurrentCount = 0;
-        mHasNextPage = false;
-    }
-
-    public final class ResultUpdater
-    {
-        private final boolean isLoadMore;
-
-        private Boolean hasNextPage = null;
+    public final class ResultUpdater {
+        private final boolean _isLoadMore;
+        private Boolean _hasNextPage = null;
         private Boolean hasData = null;
 
-        ResultUpdater(boolean isLoadMore)
-        {
-            this.isLoadMore = isLoadMore;
+        ResultUpdater(boolean isLoadMore) {
+            this._isLoadMore = isLoadMore;
         }
 
         /**
          * 设置是否有下一页
          *
          * @param hasNextPage true-是；false-否
-         * @return
          */
-        public ResultUpdater setHasNextPage(boolean hasNextPage)
-        {
-            this.hasNextPage = hasNextPage;
+        public ResultUpdater setHasNextPage(boolean hasNextPage) {
+            this._hasNextPage = hasNextPage;
             return this;
         }
 
@@ -129,39 +84,31 @@ public class FPageHolder
          * 设置是否有返回数据
          *
          * @param hasData true-是；false-否
-         * @return
          */
-        public ResultUpdater setHasData(boolean hasData)
-        {
+        public ResultUpdater setHasData(boolean hasData) {
             this.hasData = hasData;
             return this;
         }
 
         /**
          * 设置是否有返回数据
-         *
-         * @param listData
-         * @return
          */
-        public ResultUpdater setHasData(List<? extends Object> listData)
-        {
-            final boolean has = listData != null && listData.size() > 0;
-            setHasData(has);
+        public ResultUpdater setHasData(Collection<?> data) {
+            setHasData(data != null && data.size() > 0);
             return this;
         }
 
         /**
          * 更新page
          */
-        public void update()
-        {
-            if (hasNextPage == null)
+        public void update() {
+            if (_hasNextPage == null) {
                 throw new RuntimeException("you must invoke setHasNextPage() method before this");
-
-            if (hasData == null)
+            }
+            if (hasData == null) {
                 throw new RuntimeException("you must invoke setHasData() method before this");
-
-            updatePage(isLoadMore, hasNextPage, hasData);
+            }
+            updatePage(_isLoadMore, _hasNextPage, hasData);
         }
     }
 }
