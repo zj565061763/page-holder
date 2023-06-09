a336ecd0c889574a230f11204941312d85fdea38
zj565061763 <565061763@qq.com>
2023-06-09 16:04:09 +0800

    1.1.0-rc01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "page-holder"
-val libVersionName = "1.0.0-rc06"
+val libVersionName = "1.1.0-rc01"
 
 android {
     namespace = "com.sd.lib.pholder"
